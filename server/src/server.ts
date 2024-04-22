4bd6e8b161787da19109a31a0b564b1b125eb7a0
Zehan Khan <zkhan@hootboard.com>
2024-04-22 18:25:06 +0530

    fix socket cors issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import app from "./app";
 const port = 3000;
 
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
   console.log("socket connected", socket.id);
