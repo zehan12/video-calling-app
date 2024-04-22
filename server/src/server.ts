5a066721c621fa9913a92518eb36fa6eba208018
Zehan Khan <zkhan@hootboard.com>
2024-04-22 17:17:17 +0530

    create basic typescript node server

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import app from "./app";
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`
+          ################################################
+          🚀 Server listening on port: ${port} 🚀
+          ################################################
+      `);
+});
