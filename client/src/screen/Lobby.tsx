a568caa7696a3e2841ef8e5b66f161708752c7b1
Zehan Khan <zkhan@hootboard.com>
2024-04-22 18:06:51 +0530

    create lobby page

--- client/src/screen/Lobby.tsx
+++ client/src/screen/Lobby.tsx
@@ -1,7 +1,44 @@
+import { FormEvent, useCallback, useState } from "react";
+
 export const Lobby = () => {
+  const [email, setEmail] = useState("");
+  const [roomId, setRoomId] = useState("");
+
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      console.log(email, roomId);
+      return;
+    },
+    [email, roomId]
+  );
+
   return (
     <div>
       <h1>Lobby</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email ID</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          name="email"
+          placeholder="Email ID"
+        />
+        <br />
+        <label htmlFor="roomId">Room Number</label>
+        <input
+          value={roomId}
+          onChange={(e) => setRoomId(e.target.value)}
+          type="text"
+          name="roomId"
+          placeholder="Room Number"
+        />
+        <br />
+        <button type="submit" style={{ cursor: "pointer" }}>
+          Join
+        </button>
+      </form>
     </div>
   );
 };
